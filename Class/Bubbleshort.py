bb01566ffe88da6d793e7a59b79bbb04863a64d7
marcbailo <mbailo@institutmvm.cat>
2023-12-12 14:25:36 +0100

    holi

---
+++ Class/Bubbleshort.py
@@ -0,0 +1,12 @@
+#Heu de fer un codi en Python3 que ordeni el següent array de forma ascendent.
+#ordenació la fem segons la seva longitud d'ona.
+
+uwu=int(input("Selecciona un color:indigo, blau, cia, verd, groc, taronja, vermell: "))
+owo= input("Elige" ("mayor a menor o menor a mayor? "))
+array = ["indigo", "groc", "verd", "vermell", "blau", "cian", "taronja"]
+if array == "mayor a menor":
+    print("indigo, blau, cian, verd, ")
+#indigo:435, #groc:590, verd:500, vermell:625  blau:440  cian:500  taronja:590
+# indigo, blau, cian, verd, groc, taronja, vermell (ORDEN)
+auxarray = {"indigo":435,"blau":440,"cian":500,"verd":510,"groc":590,"taronja":590,"vermell":625}
+

695ec206c8ef7cad576ae1ce7a0c1527dd4bed9c
Albert <117260437+AlbertTeacher@users.noreply.github.com>
2023-12-15 12:13:44 +0100

    Notas de correccion

--- Class/estructura.py
+++ Class/estructura.py
@@ -1,7 +1,17 @@
 #PRE:Escriu un programa en Python3 que llegeixi de l’usuari una llista de tuples,
 #on cada tupla representa un estudiant amb la següent informació: (Nom, Cognom, Edat, Nota1, Nota2, Nota3).
-Alumnes=("Cuants que vols registrar")
-info=("Donem la informacio:nom,cognom,edat,nota1,nota2,nota3")
+Alumnes=("Cuants que vols registrar") # Aqui estas creando una tupla con un string dentro
+info=("Donem la informacio:nom,cognom,edat,nota1,nota2,nota3") # Aqui esta otra
+'''
+Lo que tu quieres hacer segurramente seria:
+info=input("Donem la informacio: nom, cognom, edat, nota1, nota2, nota3")
+En vez de esto haz:
+nom = input("Dona el nom: ")
+cognom = input......
+.......
+nota3 =int(input("Dona la nota3: ")
+alumne1 = (nom, cognom, ......., nota3) <----- Esto seria la tupla de un alumno
+'''
 for
 if "nota1"<=7:
     print("bona mitja")

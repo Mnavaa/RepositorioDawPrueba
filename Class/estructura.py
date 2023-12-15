60183205d6d89be69935bd9687ef1771bc355224
marcbailo <mbailo@institutmvm.cat>
2023-12-15 10:18:18 +0100

    Pre

---
+++ Class/estructura.py
@@ -0,0 +1,2 @@
+#PRE:Escriu un programa en Python3 que llegeixi de l’usuari una llista de tuples,
+#on cada tupla representa un estudiant amb la següent informació: (Nom, Cognom, Edat, Nota1, Nota2, Nota3).

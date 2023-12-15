85eb85461c2d699d4db59771988767b9c61e5e77
marcbailo <mbailo@institutmvm.cat>
2023-12-15 11:44:09 +0100

    llegando a la locura

--- Class/estructura.py
+++ Class/estructura.py
@@ -1,2 +1,11 @@
 #PRE:Escriu un programa en Python3 que llegeixi de l’usuari una llista de tuples,
 #on cada tupla representa un estudiant amb la següent informació: (Nom, Cognom, Edat, Nota1, Nota2, Nota3).
+Alumnes=("Cuants que vols registrar")
+info=("Donem la informacio:nom,cognom,edat,nota1,nota2,nota3")
+for
+if "nota1"<=7:
+    print("bona mitja")
+#if mitjana=>7:
+    #print(noc)
+#if mitjana=>8:
+    #print(noc)

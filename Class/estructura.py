f49966f956b676983af6f3ea42c8062221f560b2
marcbailo <mbailo@institutmvm.cat>
2023-12-19 11:50:38 +0100

    notafinal

--- Class/estructura.py
+++ Class/estructura.py
@@ -2,9 +2,7 @@
 #on cada tupla representa un estudiant amb la següent informació: (Nom, Cognom, Edat, Nota1, Nota2, Nota3).
 
 #Le pido al usuario cuantos alumnos quiere registar
-Alumnes=("Cuants que vols registrar")
 #Pido toda la información sobre el alumno para después poder evaluar su nota
-info=("Donem la informacio:nom,cognom,edat,nota1,nota2,nota3")
 #Quiero comparar la nota del estudiante y dependiendo de lo que tiene puede tener diferente medias de nota 
 nom= input("Donam el nom:")
 congnom=input("Donal el teu cognom:")
@@ -12,5 +10,5 @@ edat=int(input("Donm la teva edat:"))
 nota1=int(input("Donam la nota3:"))
 nota2=int(input("Donam la nota2:"))
 nota3=int(input("Dona la nota3:"))
-#if "nota1"<=7:
-   # print("bona mitja")
+
+nota_final= (nota1+0,30)*(nota2+0,40)*(nota3+0,30) 
